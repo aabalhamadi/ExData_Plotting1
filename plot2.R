54b875e27752001923a719ede6943cbbc0dba710
aabalhamadi <47100605+aabalhamadi@users.noreply.github.com>
2019-04-28 17:49:07 +0400

    Delete plot2.R

--- plot2.R
+++
@@ -1,5 +0,0 @@
-
-plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
-
-dev.copy(png,"plot2.png", width=480, height=480)
-dev.off()
